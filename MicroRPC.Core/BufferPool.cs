c48500978757c2cc77bb66a2488f1ce5d8cdd1ce
fengdingfeilong <1374084580@qq.com>
2016-12-30 16:53:21 +0800

    Add files via upload

---
+++ MicroRPC.Core/BufferPool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRPC.Core
+{
+    /// <summary>
+    /// buffer pool for tcp data buffer
+    /// </summary>
+    class BufferPool
+    {
+        private const int BUFFER_SIZE = 1460;
+        private int _bufferSize = BUFFER_SIZE;
+        private int _count;
+        private Stack<byte[]> _pool = new Stack<byte[]>();
+        private object _lockObj = new object();
+        public BufferPool(int buffersize = BUFFER_SIZE)
+        {
+            _count = Environment.ProcessorCount * 2;
+            _bufferSize = buffersize;
+            InitPool(_count, _bufferSize);
+        }
+        public BufferPool(int count, int buffersize = BUFFER_SIZE)
+        {
+            _count = count;
+            _bufferSize = buffersize;
+            InitPool(_count, _bufferSize);
+        }
+
+        private void InitPool(int count, int buffersize)
+        {
+            if (count <= 0 || buffersize <= 0) return;
+            _pool = new Stack<byte[]>(count);
+            for (int i = 0; i < count; i++)
+                _pool.Push(new byte[_bufferSize]);
+        }
+
+        public byte[] Get()
+        {
+            lock (_lockObj)
+            {
+                if (_pool.Count == 0)
+                    return new byte[_bufferSize];
+                return _pool.Pop();
+            }
+        }
+
+        public bool Recycle(byte[] buffer)
+        {
+            lock (_lockObj)
+            {
+                if (buffer == null || buffer.Length != _bufferSize) return false;
+                _pool.Push(buffer);
+                return true;
+            }
+        }
+
+        public void Clean()
+        {
+            lock (_lockObj)
+            {
+                _pool.Clear();
+            }
+        }
+
+    }
+}
--- MicroRPC.Core/TCPServer.cs
+++ MicroRPC.Core/TCPServer.cs
@@ -42,8 +42,6 @@ namespace MicroRPC.Core
     public class TCPServer
     {
         private Socket listenSocket;
-        private const int BUFFER_SIZE = 1460;
-        private byte[] buffer = new byte[BUFFER_SIZE];
 
         public event EventHandler<Socket> NewClientAccepted;
         public event EventHandler<Socket> ClientDisconnected;//client close the socket
@@ -53,6 +51,8 @@ namespace MicroRPC.Core
 
         private int _maxConnection = 10000;
         private Semaphore maxConnectionSemphore;
+
+        private BufferPool _bufferPool = new BufferPool(10000);
 
         private int _port = 9006;
         public TCPServer()
@@ -90,9 +90,11 @@ namespace MicroRPC.Core
                 {
                     lock (listenSocket)
                     {
-                        workSocket = listenSocket.EndAccept(result);
-                        if (workSocket != null && workSocket.Connected)
-                            workSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, new AsyncCallback(HandleReceive), workSocket);
+                        if (result.CompletedSynchronously)
+                            workSocket = listenSocket.Accept();
+                        else
+                            workSocket = listenSocket.EndAccept(result);
+                        BeginRecieve(workSocket);
                         maxConnectionSemphore.WaitOne();
                         listenSocket.BeginAccept(new AsyncCallback(HandleAccept), null);
                     }
@@ -105,31 +107,46 @@ namespace MicroRPC.Core
             {
                 CloseSocket(workSocket);
             }
-            catch
+            catch (Exception ex)
             {
                 throw;
             }
-            maxConnectionSemphore.Release();
             if (workSocket != null && NewClientAccepted != null) NewClientAccepted(this, workSocket);
+        }
+
+        private void BeginRecieve(Socket workSocket)
+        {
+            if (workSocket != null && workSocket.Connected)
+            {
+                byte[] buffer = _bufferPool.Get();
+                SocketDataEventArgs dataargs = new SocketDataEventArgs();
+                dataargs.WorkSocket = workSocket;
+                dataargs.Data = buffer;
+                dataargs.WorkSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(HandleReceive), dataargs);
+            }
         }
 
         private void HandleReceive(IAsyncResult result)
         {
             if (result == null || result.AsyncState == null) return;
-            var workSocket = (Socket)result.AsyncState;
-            var dataargs = new SocketDataEventArgs();
+            SocketDataEventArgs dataargs = (SocketDataEventArgs)result.AsyncState;
+            Socket workSocket = dataargs.WorkSocket;
+            byte[] buffer = dataargs.Data;
             int count = 0;
             try
             {
-                count = workSocket.EndReceive(result);
+                if (result.CompletedSynchronously)
+                    count = workSocket.Receive(buffer);
+                else
+                    count = workSocket.EndReceive(result);
                 if (count > 0)
                 {
-                    dataargs.WorkSocket = workSocket;
                     byte[] data = new byte[count];
                     Array.Copy(buffer, data, count);
                     dataargs.Data = data;
                 }
-                workSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, new AsyncCallback(HandleReceive), workSocket);
+                _bufferPool.Recycle(buffer);
+                BeginRecieve(workSocket);
             }
             catch (ObjectDisposedException ex)//server close the socket
             {
@@ -138,7 +155,7 @@ namespace MicroRPC.Core
             {
                 CloseSocket(workSocket);
             }
-            catch
+            catch (Exception ex)
             {
                 throw;
             }
@@ -150,7 +167,7 @@ namespace MicroRPC.Core
             if (args == null || args.WorkSocket == null || !args.WorkSocket.Connected || args.Data == null) return;
             try
             {
-                args.WorkSocket.BeginSend(args.Data, 0, args.Data.Length, SocketFlags.None, new AsyncCallback(HandleSend), args.WorkSocket);
+                args.WorkSocket.BeginSend(args.Data, 0, args.Data.Length, SocketFlags.None, new AsyncCallback(HandleSend), args);
             }
             catch (ObjectDisposedException ex)//server close the socket
             {
@@ -168,12 +185,17 @@ namespace MicroRPC.Core
         private void HandleSend(IAsyncResult result)
         {
             if (result == null || result.AsyncState == null) return;
-            var workSocket = (Socket)result.AsyncState;
+            SocketDataEventArgs dataargs = (SocketDataEventArgs)result.AsyncState;
+            Socket workSocket = dataargs.WorkSocket;
+            byte[] buffer = dataargs.Data;
             int count = 0;
             try
             {
-                count = workSocket.EndSend(result);
-                workSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, new AsyncCallback(HandleReceive), workSocket);
+                if (result.CompletedSynchronously)
+                    count = workSocket.Send(buffer);
+                else
+                    count = workSocket.EndSend(result);
+                workSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(HandleReceive), workSocket);
             }
             catch (ObjectDisposedException ex)//server close the socket
             {
@@ -188,7 +210,7 @@ namespace MicroRPC.Core
             }
             if (DataSended != null) DataSended(this, count);
         }
-        
+
         private void CloseSocket(Socket socket)
         {
             if (socket != null)
@@ -202,6 +224,7 @@ namespace MicroRPC.Core
                 catch { }
                 socket.Close();
                 socket = null;
+                maxConnectionSemphore.Release();
             }
         }
 
